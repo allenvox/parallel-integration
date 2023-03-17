47a6eb22f6e56ffcc534d03f4aa3fe9cab0a2f0b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-03-17 17:23:36 +0700

    Add getrand function

--- montecarlo.c
+++ montecarlo.c
@@ -4,8 +4,13 @@
 const double PI = 3.14159265358979323846;
 const int n = 10000000; // 10 mil
 
+// variant 1
 double func(double x, double y) {
     return x / (y * y);
+}
+
+double getrand(unsigned int *seed) {
+    return (double)rand_r(seed) / RAND_MAX;
 }
 
 int main () {
@@ -21,8 +26,8 @@ int main () {
         unsigned int seed = omp_get_thread_num();
         #pragma omp for nowait
         for (int i = 0; i < n; i++) {
-            double x = getrand(&seed) * PI; // x in [0, pi]
-            double y = getrand(&seed);      // y in [0, sin(x)]
+            double x = getrand(&seed);         // x in [0, 1]
+            double y = getrand(&seed) * 7 - 2; // y in [2, 5]
             if (y <= sin(x)) {
                 in_loc++;
                 s_loc += func(x, y);
